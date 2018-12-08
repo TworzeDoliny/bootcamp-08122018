5acfb10468bb2369d10f4788c8f3031064570f5c
TworzeDoliny <m.nazarek@wp.pl>
2018-12-08 16:44:38 +0100

    Dzień pierwszy

---
+++ first.py
@@ -0,0 +1,35 @@
+print("Hello world")
+
+a = "2"
+b = None
+b = True 
+
+print(1 + 1)
+
+print( a + "5" )
+
+print(type(a))
+
+print(type(1.2))
+
+print(type(4))
+
+print(type(False)) # True
+
+print(type(None)) # True
+# print("AAAA" - "BUBIUS")
+print(1 + 4)
+print(1 - 8.8)
+
+print("CBA" * 2)
+print(1 // 3)
+print(4 % 2)
+
+print( 3 ** 2)
+print(9 ** (1/2))
+
+print(1 == 1)
+print(2 < 3)
+print(4 < 2)
+
+
---
+++ zadanie_1.py
@@ -0,0 +1,23 @@
+# Policz pole trojkata o podsstawie 10 i wys 5
+a = 10
+b = 5
+c = a*b/2
+print("Pole trójkąta wynosi: ", c)
+
+# Policz pole koła o promieniu 7
+r = 7
+P = 3.14 * r ** 2
+print(" Pole koła wynosi: ", P)
+
+# Policz pole trapezu o dł podstaw 3 i 9 oraz wys 5
+x = 3
+y = 9
+h = 5
+P =(x + y) /2 * h
+print("Pole trapezu wynosi: ", P)
+
+
+# Policz objętość kuli o zadanym promieniu
+r = 10
+V= 4/3 * 3.14 * r**3
+print(" Objętość kuli o zadanym promieniu wynosi: ", V)
