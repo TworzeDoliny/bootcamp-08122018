84b06a71cf7c57cafdefbc547dd931da83051114
TworzeDoliny <m.nazarek@wp.pl>
2018-12-09 16:57:53 +0100

    cd

--- kolekcje/zadanie_6.py
+++ kolekcje/zadanie_6.py
@@ -1,11 +1,26 @@
-liczby = [5, 2, 8, 4, 1]
+# liczby = [5, 2, 8, 4, 1]
+#
+# temp = liczby[0]
+# liczby[0] = liczby[2]
+# liczby[2] = temp
+#
+# liczby[0], liczby[2] = liczby[2], liczby[0]
+#
+# for i in range(len(liczby)):
+#     print(i)
+#
 
-temp = liczby[0]
-liczby[0] = liczby[2]
-liczby[2] = temp
+liczby = [5, 2, 1, 4, 3]
 
-liczby[0], liczby[2] = liczby[2], liczby[0]
+indeks_min, indeks_max = None, None
 
-for i in range(len(liczby)):
-    print(i)
+for indeks in range(len(liczby)):
 
+    if indeks_min is None or liczby[indeks] < liczby[indeks_min]:
+        indeks_min = indeks
+    if indeks_max is None or liczby[indeks] > liczby[indeks_max]:
+        indeks_max = indeks
+
+liczby[indeks_max], liczby[indeks_min] = liczby[indeks_min], liczby[indeks_max]
+
+assert liczby
